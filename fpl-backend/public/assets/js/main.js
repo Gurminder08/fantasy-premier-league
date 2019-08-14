7c848f02767bbf0b6905cccec0a5cbdaa3685bc6
gurminder <gurminder95@gmail.com>
2019-08-14 01:21:52 -0300

    adding public assets

---
+++ fpl-backend/public/assets/js/main.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', ()=>{
+//     const response = fetch('/bootstrap-static', 
+//         {
+//          method: 'GET',
+//          headers: {
+//              'Content-Type': 'application/json'
+//          }
+//         });
+//     response.then((resp)=>{
+//         resp.json().then((data)=>{
+//             console.log(data);
+//         });
+//     });
+// });
