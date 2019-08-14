afda9f81d623133d1f25f9cc2eb0f8283a07c902
gurminder <gurminder95@gmail.com>
2019-08-14 01:33:09 -0300

    adding api

---
+++ fpl-backend/controllers/controller.js
@@ -0,0 +1,17 @@
+let express = require("express");
+const axios = require("axios");
+let router = express.Router();
+
+router.get("/", (req,res)=>{
+    console.log(req.url);
+    res.render("index");
+});
+
+router.get("/bootstrap-static", (req,res)=>{
+    const response = axios.get('https://fantasy.premierleague.com/api/bootstrap-static');
+    response.then((resp)=>{
+        res.json(resp.data);
+    });
+})
+
+module.exports = router;
